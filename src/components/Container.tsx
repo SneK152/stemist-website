57fe46105634b3884e35d75b48a3313eb760e7aa
snek152 <kakanisnehil@gmail.com>
2022-01-07 10:58:43 -0800

    tweaking homepage (image)

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 
 export default function Container({ children }: { children: JSX.Element | string }) {
+    const location = useLocation()
     useEffect(() => {
         window.onscroll = () => {
             if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
@@ -11,6 +13,9 @@ export default function Container({ children }: { children: JSX.Element | string
             }
         }
     }, [])
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [location.pathname])
     return <div className="mx-auto h-[10000px]">
         <Navbar className="sm:px-6 lg:px-6 px-2 transition-[top] duration-300 z-50 sticky top-[-56px] bg-gradient-to-r from-slate-200 to-slate-300 backdrop-filter backdrop-blur" />
         <div className="sm:px-6 max-w-[100rem] mx-auto px-2 lg:px-6">
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,15 @@
+const image = "https://www.cae.net/wp-content/uploads/2018/03/dexway-classroom-companion-ingles-uk.jpg"
+
 export default function Index() {
-  return <h1>Homepage</h1>
+  return <div>
+    <div className="h-[calc(100vh-56px)] object-cover bg-no-repeat bg-cover" style={{ backgroundImage: `url("${image}")` }}>
+      <div className="w-full h-full backdrop-blur-sm flex items-center justify-center">
+        <div className="absolute h-2/5 w-1/2 text-center flex flex-col justify-center gap-3 bg-opacity-75">
+          <h1 className="font-bold text-5xl">STEMzyme</h1>
+          <hr className=" border-black" />
+          <p className="font-display text-xl">Filler content here..........</p>
+        </div>
+      </div>
+    </div>
+  </div>
 }
