b3a767f5c801572312a928455ece79df86e872c9
Snehil Kakani <kakanisnehil@gmail.com>
2022-01-21 17:13:19 -0800

    adding localstorage hook and code to prevent dupes

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,17 +1,25 @@
-import React from "react"
+import React, { ReactElement, useState } from "react"
 import { useRef } from "react"
+import { useLocalStorage } from "../utils"
+import { CheckIcon, XIcon } from "@heroicons/react/outline"
+import Spinner from "../components/Spinner"
+import { useEffect } from "react"
 
 export default function Signup() {
     const email = useRef<HTMLInputElement>(null)
     const name = useRef<HTMLInputElement>(null)
     const hearsay = useRef<HTMLInputElement>(null)
+    const [localEmail, setEmail] = useLocalStorage("email", null)
+    const [submit, setSubmit] = useState<ReactElement>(<h1>Submit</h1>)
     const formSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
-        if (email.current && name.current && hearsay.current) {
+        setSubmit(<Spinner color="white" className="w-5 h-5 m-auto" />)
+        if (email.current && name.current && hearsay.current && localEmail !== email.current.value) {
             const formdata = new URLSearchParams()
             formdata.append("entry.187006080", email.current.value) // Email
             formdata.append("entry.782191400", hearsay.current.value) // How did you find out
             formdata.append("entry.479709865", name.current.value) // Name
+            setEmail(email.current.value)
             await fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLSe4XsTBN_6-RJhl1DXkLdvu3dkaP3SjydLVslHD4nHRhKMUfA/formResponse", {
                 method: "POST",
                 headers: {
@@ -19,8 +27,22 @@ export default function Signup() {
                 },
                 body: formdata.toString(),
             })
+            setTimeout(() => {
+                setSubmit(<CheckIcon height={20} width={20} className="m-auto" />)
+                setTimeout(() => setSubmit(<h1>Submit</h1>), 1200)
+            }, 500)
+
+        } else if (localEmail === email.current?.value) {
+            setSubmit(<div className="m-auto flex">
+                <XIcon height={20} width={20} className="m-auto inline-block" />
+                <p>Duplicate user.</p>
+            </div>)
+            setTimeout(() => setSubmit(<h1>Submit</h1>), 2000)
         }
     }
+    useEffect(() => {
+        return clearTimeout
+    }, [])
     return <div>
         <h1 className="text-center text-xl font-bold font-display">Signup</h1>
         <form onSubmit={formSubmit} className="max-w-2xl mx-auto px-2 sm:px-6 lg:px-8 py-3 space-y-3">
@@ -63,7 +85,9 @@ export default function Signup() {
                 placeholder="Where did you hear about us?"
             />
 
-            <button type="submit" className="relative w-full sm:w-1/2 md:w-1/4 m-auto block py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-night hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-night">Submit</button>
+            <button type="submit" className="relative w-full sm:w-1/2 md:w-1/4 m-auto block py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-night hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-night">
+                {submit}
+            </button>
         </form>
     </div>
 }
