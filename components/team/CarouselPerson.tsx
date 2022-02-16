9497b97292649e4a2cef65075864fe1020463012
snek152 <kakanisnehil@gmail.com>
2022-02-16 10:01:22 -0800

    image tweak on carousel

--- components/team/CarouselPerson.tsx
+++ components/team/CarouselPerson.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 export default function CarouselPerson({ person }: { person: Person }) {
   return (
     <div className="flex px-10">
-      <div className="w-1/4 relative">
+      <div className="w-1/2 h-full relative pl-10 grid p-5">
         <Image
           src={person.image}
           alt="Mentor spotlight"
-          width={60}
-          height={60}
+          width={100}
+          height={100}
           layout="responsive"
-          className="rounded-3xl"
+          className="rounded-full object-cover"
         />
       </div>
-      <div>
-        <h1>{person.name}</h1>
-        <p>{person.description}</p>
+      <div className="py-5 pl-10">
+        <h1 className="text-5xl font-display">{person.name}</h1>
+        <p className="max-w-[80ch]">{person.description}</p>
       </div>
     </div>
   );
