77afd9059bb3631d9ff4c20387b0b89f60e224d1
snek152 <kakanisnehil@gmail.com>
2022-03-05 15:46:51 -0800

    fixing require bug

--- lib/imageHelpers.ts
+++ lib/imageHelpers.ts
@@ -1,5 +1,5 @@
 export const resolveTeamImage = (name?: string, fileType?: string) => {
   return name
-    ? require(`@/public/team/${name}.${fileType}`)
-    : require("@/public/avatar.svg");
+    ? require(`@/public/team/${name}.${fileType}`).default.src
+    : require("@/public/avatar.svg").default.src;
 };
