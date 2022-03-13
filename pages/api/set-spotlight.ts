8ff90f8a980ac8c6f81e09f2d8c1f16e53982649
SneK152 <kakanisnehil@gmail.com>
2022-03-12 18:59:05 -0800

    updating spotlight API

---
+++ pages/api/set-spotlight.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import db from "@/lib/serverApp";
+import Person from "@/lib/types/Person";
+import teachers from "@/lib/data/teachers";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const get = await db.collection("spotlight").doc("spotlight").get();
+  const data: Person[] = get.data()?.spotlight;
+  const updatedData = data.map((p) => teachers.find((e) => e.name === p.name));
+  await db.collection("spotlight").doc("spotlight").set({
+    spotlight: updatedData,
+  });
+  res.status(200).json({});
+}
