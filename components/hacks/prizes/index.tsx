bd06fe686da1b489ff7f623cd894cca3931c3c93
snek152 <kakanisnehil@gmail.com>
2022-03-14 11:02:58 -0700

    various bugfixes

--- components/hacks/prizes/index.tsx
+++ components/hacks/prizes/index.tsx
@@ -1,21 +1,20 @@
-import {useMemo} from 'react'
-import Dynamic from 'next/dynamic'
-import prizes from './prizes'
+import { useMemo } from "react";
+import Dynamic from "next/dynamic";
+import prizes from "./prizes";
 
-const Section = Dynamic(() => import('@/components/hacks/layout/Section'))
-const Prize = Dynamic(() => import('./prize'))
+const Section = Dynamic(() => import("@/components/hacks/layout/Section"));
+const Prize = Dynamic(() => import("./prize"));
 
 export default function PrizesSection() {
-    
-    const PrizeList = useMemo(() => prizes, [prizes])
+  const PrizeList = useMemo(() => prizes, []);
 
-    return (
-        <Section name='Prizes'>
-            <div className='odd:bg-gray-400 even:bg-gray-200 rounded-lg'>
-                {PrizeList.map(props => (
-                    <Prize {...props} />
-                ))}
-            </div>
-        </Section>
-    )
+  return (
+    <Section name="Prizes">
+      <div className="odd:bg-gray-400 even:bg-gray-200 rounded-lg">
+        {PrizeList.map((props, k) => (
+          <Prize {...props} key={k} />
+        ))}
+      </div>
+    </Section>
+  );
 }
--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -45,10 +45,6 @@ export default function Container({
         <meta name="msapplication-TileColor" content="#2B5797" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Antic&family=Exo:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-          rel="stylesheet"
-        />
         <title>STEMist Education | {title}</title>
       </Head>
       <Navbar />
