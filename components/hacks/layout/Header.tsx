c9ae4a85284f2b6275e903cc547c265767b5d847
Jacob Fernandes <jacobvg12345@gmail.com>
2022-03-14 14:07:48 -0700

    Signup-Button

--- components/hacks/layout/Header.tsx
+++ components/hacks/layout/Header.tsx
@@ -3,10 +3,23 @@ import Link from "next/link";
 import Button from "./Button";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 
+function Description() {
+  return (
+    <div className="py-3 px-2">
+      <p className="text-lg font-semibold" >From June 24th to June 26th</p>
+      <button className="text-white bg-blue-400 animate-bounce duration-200 mt-5 py-3 px-6 rounded-lg font-semibold">
+        Sign up!
+      </button>
+    </div>
+  )
+}
+
 export default function Header() {
   return (
     <div className='w-full h-full p-5 text-center center justify-center'>
-      <PartialBanner title="STEMist Hacks" />
+      <PartialBanner title="STEMist Hacks">
+        <Description />
+      </PartialBanner>
       <div className='justify-center text-center m-auto'>
           <ChevronDownIcon className="mx-auto justify-center h-10 animate-bounce" />
       </div>
