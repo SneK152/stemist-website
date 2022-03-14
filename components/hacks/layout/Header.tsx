934cc1681a03098528989c793bd2f36b18e678c4
Jacob Fernandes <jacobvg12345@gmail.com>
2022-03-13 23:52:43 -0700

    Suspense

--- components/hacks/layout/Header.tsx
+++ components/hacks/layout/Header.tsx
@@ -1,7 +1,15 @@
 import PartialBanner from "@/components/layout/PartialBanner";
 import Link from "next/link";
 import Button from "./Button";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 
 export default function Header() {
-  return <PartialBanner title="STEMist Hacks" />;
+  return (
+    <div className='w-full h-full p-5 text-center center justify-center'>
+      <PartialBanner title="STEMist Hacks" />
+      <div className='justify-center text-center m-auto'>
+          <ChevronDownIcon className="mx-auto justify-center h-10 animate-bounce" />
+      </div>
+    </div>
+  );
 }
