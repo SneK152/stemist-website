94240217e4654b96735ed1726be690c470e6811a
snek152 <kakanisnehil@gmail.com>
2022-03-15 22:16:09 -0700

    updating aos animation

--- components/useAos.tsx
+++ components/useAos.tsx
@@ -1,10 +1,31 @@
 import Aos from "aos";
 
-let animations = ["fade", "fade-up", "fade-down", "flip-up", 'flip-down', 'slide-up', 'slide-down']
+let animations = [
+  "fade",
+  "fade-up",
+  "fade-down",
+  "flip-up",
+  "flip-down",
+  "slide-up",
+  "slide-down",
+];
 
-export default function useAos(transition?: 'fade' | 'fade-up' | 'fade-down' | 'flip-up' | 'flip-down' | 'slide-up' | 'slide-down') {
-    
-    let exports = {'data-aos': transition ? transition : 'ease-in', 'data-aos-offset': "200", "data-aos-delay": "50", "data-aos-duration": "1000"}
+export default function useAos(
+  transition?:
+    | "fade"
+    | "fade-up"
+    | "fade-down"
+    | "flip-up"
+    | "flip-down"
+    | "slide-up"
+    | "slide-down"
+) {
+  let exports = {
+    "data-aos": transition ? transition : "fade-up",
+    // "data-aos-offset": "200",
+    // "data-aos-delay": "50",
+    // "data-aos-duration": "1000",
+  };
 
-    return exports
+  return exports;
 }
