d7b842f28aced8ccbeeb2fcf2ef2877b5269859e
SneK152 <kakanisnehil@gmail.com>
2022-03-20 13:42:12 -0700

    tweaking hacks website slightly

--- components/hacks/prizes/index.tsx
+++ components/hacks/prizes/index.tsx
@@ -3,9 +3,12 @@ import prizes from "./prizes";
 import Section from "@/components/hacks/layout/Section";
 import Prize from "./prize";
 import useAos from "@/components/useAos";
+import useWindowSize from "@/lib/hooks/useWindowSize";
 
 export default function PrizesSection() {
   const PrizeList = useMemo(() => prizes, []);
+
+  const { width } = useWindowSize();
 
   return (
     <Section name="Prizes">
@@ -13,9 +16,9 @@ export default function PrizesSection() {
         {...useAos()}
         className="text-black odd:bg-gray-400 even:bg-gray-200 rounded-lg w-full"
       >
-        <div className="w-full grid grid-cols-12 py-3">
-          <span className="col-span-1"></span>
-          <p className="w-full col-span-5 text-xl font-display font-semibold">
+        <div className="w-full grid grid-cols-12 py-3 px-5">
+          {width! > 640 && <span className="col-span-1"></span>}
+          <p className="w-full col-span-6 sm:col-span-5 text-xl font-display font-semibold">
             Position
           </p>
           <p className="w-full col-span-6 text-xl font-display font-semibold">
@@ -27,7 +30,7 @@ export default function PrizesSection() {
           {PrizeList.map((props, k) => (
             <div
               key={k}
-              className="grid grid-cols-12 items-center group py-1 transition-all hover:bg-gray-300 hover:rounded-lg"
+              className="grid grid-cols-12 items-center group py-1 transition-all px-5 hover:bg-gray-300"
             >
               <Prize {...props} />
             </div>
