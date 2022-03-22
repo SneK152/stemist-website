5169cc05dc1695e58dd075f3c472722740592dce
snek152 <kakanisnehil@gmail.com>
2022-03-22 12:59:25 -0700

    removing unnecessary import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 import "../styles/index.css";
 import { AppProps } from "next/app";
-import Container from "@/components/layout/Container";
 import AOS from "aos";
 import { useEffect } from "react";
 import "aos/dist/aos.css";
-import "plyr-react/dist/plyr.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
