e645be56393580748874c08197cd2c031a5b2fe1
snek152 <kakanisnehil@gmail.com>
2022-03-23 16:08:16 -0700

    adding polyfill

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import { AppProps } from "next/app";
 import AOS from "aos";
 import { useEffect } from "react";
 import "aos/dist/aos.css";
+import smoothscroll from "smoothscroll-polyfill";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     AOS.init({
       once: true,
     });
+    smoothscroll.polyfill();
   }, []);
   return <Component {...pageProps} />;
 };
