69e556d7e918de79f129ac1da12b5fb535ad701e
snek152 <kakanisnehil@gmail.com>
2022-03-24 18:25:12 -0700

    decreasing size of quote

--- components/layout/AboutSection.tsx
+++ components/layout/AboutSection.tsx
@@ -24,18 +24,18 @@ export default function Section({
   return (
     <>
       <div className="justify-center grid grid-cols-9 w-12/13 min-h-[30rem] font-display bg-white px-6 text-elipsis py-3 rounded-sm gap-3">
-        <div className="lg:col-span-5 col-span-8 my-auto">
+        <div className="lg:col-span-5 col-span-9 my-auto">
           <h1 className="text-4xl font-bold py-2">{title}</h1>
           <p className="text-xl font-sans text-clip">{children}</p>
           <br />
-          <p className="col-span-8 font-writing h-auto text-left">
+          <p className="col-span-9 font-writing h-auto text-left">
             <span className="block italic text-sm sm:text-lg">
               &quot;{quote}&quot;
             </span>
             <span className="block text-xs sm:text-base">-{author}</span>
           </p>
         </div>
-        <div className="lg:col-span-4 col-span-8 relative gap-1 flex flex-col border border-black border-opacity-50">
+        <div className="lg:col-span-4 col-span-9 relative gap-1 flex flex-col border border-black border-opacity-50">
           <div className="h-80 lg:h-full w-full relative">
             <Image
               src={image}
@@ -44,7 +44,7 @@ export default function Section({
               objectFit="cover"
             />
           </div>
-          <caption className="text-lg font-writing text-gray-600">
+          <caption className="text-base p-1 font-writing text-gray-600">
             {caption}
           </caption>
         </div>
