a2ad4cf8c8d2319f14913d829400416ce9b251b6
snek152 <kakanisnehil@gmail.com>
2022-03-24 19:17:19 -0700

    updating FAQ

--- components/hacks/faq/Question.tsx
+++ components/hacks/faq/Question.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { animated, useSpring } from "react-spring";
+
 export default function Question({
   answer,
   question,
@@ -5,16 +8,29 @@ export default function Question({
   answer: string;
   question: string;
 }) {
+  const [open, setOpen] = useState(false);
+  const answerSpring = useSpring({
+    height: open ? "6rem" : "0rem",
+    backgroundColor: !open ? "transparent" : "white",
+  });
   return (
     <div className="rounded-md flex flex-col">
-      <div className="rounded-lg text-2xl rounded-b-none text-left flex pl-2 py-3 bg-gray-900 bg-opacity-90 font-display">
+      <div
+        className={`rounded-lg text-2xl ${
+          open ? "rounded-b-none" : ""
+        } text-left flex h-20 pl-2 py-3 transition-all bg-blue-500 bg-opacity-60 backdrop-blur-sm font-display cursor-pointer`}
+        onClick={() => setOpen((o) => !o)}
+      >
         {question}
       </div>
-      <div
-        className={`rounded-lg pl-2 font-writing py-3 h-full rounded-t-none bg-gray-200 text-black`}
+      <animated.div
+        style={answerSpring}
+        className={`rounded-lg pl-2 ${
+          open ? "opacity-100" : "opacity-0 select-none"
+        } font-writing transition-opacity rounded-t-none bg-gray-200 text-black`}
       >
         {answer}
-      </div>
+      </animated.div>
     </div>
   );
 }
