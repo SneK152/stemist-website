8db77f5c2d7c272d498dcf5939eb1746bc9e6eb4
Jacob Fernandes <jacobvg12345@gmail.com>
2022-04-04 23:56:38 -0700

    Adding Form

--- pages/hacks/signup.tsx
+++ pages/hacks/signup.tsx
@@ -1,5 +1,16 @@
 import SignupForm from "@/components/hacks/signup";
+import Container from "@/components/layout/Container";
+import Header from "@/components/hacks/layout/Header";
+import { hackathonNavLinks, navLinks } from "@/lib/data/navLinks";
 
 export default function SignupPage() {
-  return <SignupForm />
+  return (
+    <Container title="Sign Up" customNav={hackathonNavLinks} navTitle="Sign Up" noNav>
+      <div className="h-screen">
+        <div className="m-5 sticky h-5/6">
+          <SignupForm className='mr-10 shadow-xl rounded-md border border-gray-400 ' />
+        </div>
+      </div>
+    </Container>
+  )
 }
