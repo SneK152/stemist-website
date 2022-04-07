406bc24cd98028c218c4b7d495c102b9c4721582
Jacob Fernandes <jacobvg12345@gmail.com>
2022-04-07 00:05:39 -0700

    Hackathon Buttons

--- pages/hacks/index.tsx
+++ pages/hacks/index.tsx
@@ -6,7 +6,12 @@ import Schedule from "@/components/hacks/schedule/Schedule";
 import Container from "@/components/layout/Container";
 import PrizesSection from "@/components/hacks/prizes";
 import { hackathonNavLinks } from "@/lib/data/navLinks";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps } from 'next'
+
+const HACKATHON_INFO = {
+  signup_form: "https://docs.google.com/forms/d/e/1FAIpQLSdPbxLj32YvUdPLnmtlYhhRXFiGwPCPuHK7Vf7e89DMAFsdew/viewform",
+  devpost: "https://stemist-hacks.devpost.com/"
+}
 
 export default function App() {
   return (
@@ -19,16 +24,24 @@ export default function App() {
       <Header />
       <div className="max-w-[100rem] px-2 sm:px-6 lg:px-6 m-auto">
         <Section name="About our Hackathon" id="about">
-          <div className="bg-black bg-opacity-20 text-white text-lg p-5 rounded-lg shadow-md">
+          <div className="bg-black bg-opacity-20 text-white text-lg py-5 rounded-lg shadow-md">
             Welcome to STEMist Hacks! Join other hackers in creating a project
             to impress judges, and to win prizes. There will be workshops
             provided from our gracious sponsors, volunteers, and guest speakers
             to aid in your process. Or, you can start hacking the moment STEMist
             Hacks is open!
+            <div className='space-x-5 pt-4'>
+              <a href={HACKATHON_INFO.signup_form} className='px-5 py-2 bg-white text-black font-semibold rounded-md hover:bg-slate-200'>
+                Sign Up
+              </a>
+              <a href={HACKATHON_INFO.devpost} className='px-5 py-2 text-white border-2 font-semibold border-white rounded-md hover:bg-slate-900 hover:border-slate-200'>
+                Devpost
+              </a>
+            </div>
           </div>
         </Section>
-        <Section name="Requirements">
-          <div className="bg-black bg-opacity-20 text-white text-lg p-5 rounded-lg shadow-md">
+        <Section name="Ideas to Build">
+          <div className="bg-black bg-opacity-20 text-white text-lg py-5 rounded-lg shadow-md">
             Build anything {`"`}tech-related{`"`} to impress judges and win
             prizes. This can include- AR/VR software, Camera Software, Data
             Analysis, Computer Science Programs, Games, Blender Videos,
@@ -36,6 +49,14 @@ export default function App() {
             Pneumatic powered systems, a solution to a problem with an MVP
             coded, and much, much, more! If you have any questions, please ask
             on our discord server.
+          </div>
+        </Section>
+        <Section name="Requirements to Attend">
+          <div className="bg-black bg-opacity-20 text-white text-lg py-5 rounded-lg shadow-md">
+            Due to restrictions from DevPost, if you are above 13 years old you can come but if you are younger, you will need parent permission especially 
+            parent supervisin while filing out the forms.
+
+            Thanks in Advance for complying to this rule
           </div>
         </Section>
         <Section name="Hackathon Agenda (PST)">
@@ -53,11 +74,11 @@ export default function App() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  return {
-    redirect: {
-      destination: "https://stemist-hacks.devpost.com",
-      permanent: false,
-    },
-  };
-};
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   return {
+//     redirect: {
+//       destination: "https://stemist-hacks.devpost.com",
+//       permanent: false,
+//     },
+//   };
+// };
