86dbf53c6aaa753684bba5163ef35cd036cd4ba8
snek152 <kakanisnehil@gmail.com>
2022-04-08 16:46:14 -0700

    adding prize sections

--- components/hacks/prizes/prize.tsx
+++ components/hacks/prizes/prize.tsx
@@ -1,28 +1,21 @@
 import React from "react";
-import { GiftIcon } from "@heroicons/react/outline";
 import useHover from "./useHover";
-import useWindowSize from "@/lib/hooks/useWindowSize";
 
 export interface PrizeProps {
   name: string;
   prizes: string;
   presented_by: string;
+  type: "winners" | "everyone" | "raffle";
 }
 
 export default function Prize({ name, prizes, presented_by }: PrizeProps) {
   let { hoverOptions, status } = useHover(presented_by, prizes);
-  const { width } = useWindowSize();
   return (
     <div
       className="grid grid-cols-12 items-center group py-1 transition-all px-5 hover:bg-black hover:bg-opacity-10"
       {...hoverOptions}
     >
-      {width! > 640 && (
-        <div className="col-span-1">
-          <GiftIcon className="h-12 p-2 rounded-full shadow-md" />
-        </div>
-      )}
-      <div className="sm:col-span-5 col-span-6 font-semibold">{name}</div>
+      <div className="col-span-6 font-semibold">{name}</div>
       <div className="col-span-6">{status}</div>
     </div>
   );
--- components/hacks/prizes/prizes.ts
+++ components/hacks/prizes/prizes.ts
@@ -5,31 +5,37 @@ let prizes: PrizeProps[] = [
     prizes: "Lifetime Taskade Subscription",
     name: "Top Three",
     presented_by: "Taskade",
+    type: "winners",
   },
   {
     prizes: "Five Year Taskade Subscription",
     name: "10x",
     presented_by: "Taskade",
+    type: "everyone",
   },
   {
     prizes: "Echo3D Business Plan Subscription",
     name: "10x",
     presented_by: "Echo3D",
+    type: "everyone",
   },
   {
     prizes: "Desmos T-Shirt and Socks",
     name: "Raffle",
     presented_by: "Desmos",
+    type: "raffle",
   },
   {
     prizes: "Two Year NordVPN Subscription",
     name: "2x",
     presented_by: "NordVPN",
+    type: "everyone",
   },
   {
-    prizes: "Wolfram|One and Wolfram|Alpha Pro",
-    name: "3x",
+    prizes: "Wolfram | One and Wolfram | Alpha Pro",
+    name: "Top Three",
     presented_by: "Wolfram",
+    type: "winners",
   },
 ];
 
