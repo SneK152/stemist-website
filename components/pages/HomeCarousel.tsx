56bf8a4f2d917b4100b4ebabcac024e535c7ecfa
snek152 <kakanisnehil@gmail.com>
2022-05-22 16:29:56 -0700

    adding carousel images

--- components/pages/HomeCarousel.tsx
+++ components/pages/HomeCarousel.tsx
@@ -2,7 +2,14 @@ import { useSpringCarousel } from "react-spring-carousel";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 
-const images: string[] = ["/about/teamphoto.jpg", "/about/session.png"];
+const images: string[] = [
+  "/about/carousel2.png",
+  "/about/carousel5.png",
+  "/about/carousel1.png",
+  "/about/carousel4.png",
+  "/about/carousel3.png",
+  "/about/carousel6.png",
+];
 
 export default function HomeCarousel() {
   const { carouselFragment, slideToPrevItem, slideToNextItem } =
@@ -30,14 +37,14 @@ export default function HomeCarousel() {
         className="absolute h-full top-0 bottom-0 left-0 z-[1] flex items-center justify-center sm:w-[10%] w-[15%] p-0 text-white text-center bg-none border-0"
         onClick={slideToPrevItem}
       >
-        <ChevronLeftIcon className="w-9 h-9 p-1" />
+        <ChevronLeftIcon className="w-9 h-9 p-1 bg-black rounded-xl shadow-md" />
       </button>
       {carouselFragment}
       <button
         className="absolute h-full top-0 bottom-0 right-0 z-[1] flex items-center justify-center sm:w-[10%] w-[15%] p-0 text-white text-center bg-none border-0"
         onClick={slideToNextItem}
       >
-        <ChevronRightIcon className="w-9 h-9 p-1" />
+        <ChevronRightIcon className="w-9 h-9 p-1 bg-black rounded-xl shadow-md" />
       </button>
     </div>
   );
