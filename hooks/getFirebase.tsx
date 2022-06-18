00a196810a51460c33761d6adf9d76ddedac9f91
Jacob Fernandes <59456400+proballstar@users.noreply.github.com>
2022-06-18 10:22:44 -0700

    Auth Update Firebase Config

--- hooks/getFirebase.tsx
+++ hooks/getFirebase.tsx
@@ -1,6 +1,6 @@
-import {} from 'firebase/app'
+import {FirebaseOptions, initializeApp, getApp, getApps} from 'firebase/app'
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
     apiKey: "AIzaSyCQmRfJIzq67VlnvvZdpHonpHeW1hKRq4c",
     authDomain: "stemist-c71a6.firebaseapp.com",
     projectId: "stemist-c71a6",
@@ -10,6 +10,7 @@ const firebaseConfig = {
 };
 
 export default function getFirebase() {
-    return 
+    const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
+    return app
 }
 
