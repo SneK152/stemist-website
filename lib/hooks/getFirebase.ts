3df164619868b3ff091a5d1e7e1fb9bbb624a2e6
snek152 <kakanisnehil@gmail.com>
2022-06-19 10:30:46 +0530

    format and fix bug

--- lib/hooks/getFirebase.ts
+++ lib/hooks/getFirebase.ts
@@ -1,16 +1,15 @@
-import {FirebaseOptions, initializeApp, getApp, getApps} from 'firebase/app'
+import { FirebaseOptions, initializeApp, getApp, getApps } from "firebase/app";
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: "AIzaSyCQmRfJIzq67VlnvvZdpHonpHeW1hKRq4c",
-    authDomain: "stemist-c71a6.firebaseapp.com",
-    projectId: "stemist-c71a6",
-    storageBucket: "stemist-c71a6.appspot.com",
-    messagingSenderId: "249941894665",
-    appId: "1:249941894665:web:dcac5a6d3db7e41d86bad8"
+  apiKey: "AIzaSyCQmRfJIzq67VlnvvZdpHonpHeW1hKRq4c",
+  authDomain: "stemist-c71a6.firebaseapp.com",
+  projectId: "stemist-c71a6",
+  storageBucket: "stemist-c71a6.appspot.com",
+  messagingSenderId: "249941894665",
+  appId: "1:249941894665:web:dcac5a6d3db7e41d86bad8",
 };
 
 export default function getFirebase() {
-    const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
-    return app
+  const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+  return app;
 }
-
