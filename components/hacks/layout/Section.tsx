aac395b1a3e8f27cf8499b4f549b1b9afc8f54fc
Yash-Singh1 <saiansh2525@gmail.com>
2022-06-21 13:24:25 -0500

    fix: leftover inverted work on section comp

--- components/hacks/layout/Section.tsx
+++ components/hacks/layout/Section.tsx
@@ -7,7 +7,7 @@ interface SectionProps {
 
 export default function Section({ children, name, id, inverted = false }: SectionProps) {
   return (
-    <section className={`px-5 scroll-mt-24 mt-5" {...(id ? { id: id } : {})}>
+    <section className={`px-5 scroll-mt-24 mt-5 ${inverted ? 'text-white bg-[#1d263b]' : ''}`} {...(id ? { id: id } : {})}>
       <h1 className="text-4xl py-4 capitalize font-bold font-display text-center">
         {name}
       </h1>
