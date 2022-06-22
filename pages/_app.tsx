1da30b2d655ca9b52b08ea67c4ca25a6b073aab5
snek152 <kakanisnehil@gmail.com>
2022-06-22 10:48:33 +0530

    removing aos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,24 @@
 import "../styles/index.css";
 import { AppProps } from "next/app";
-import AOS from "aos";
 import { useEffect } from "react";
-import "aos/dist/aos.css";
 import smoothscroll from "smoothscroll-polyfill";
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => {
   useEffect(() => {
-    const callback = () => {
-      AOS.refresh();
-    };
-    AOS.init({
-      once: true,
-      easing: "ease-in-out-sine",
-      duration: 400,
-    });
+    // const callback = () => {
+    //   AOS.refresh();
+    // };
+    // AOS.init({
+    //   once: true,
+    //   easing: "ease-in-out-sine",
+    //   duration: 400,
+    // });
     smoothscroll.polyfill();
-    router.events.on("routeChangeComplete", callback);
-    return () => {
-      router.events.off("routeChangeComplete", callback);
-    };
-  }, [router.events]);
+    // router.events.on("routeChangeComplete", callback);
+    // return () => {
+    //   router.events.off("routeChangeComplete", callback);
+    // };
+  }, []);
   return <Component {...pageProps} />;
 };
 
