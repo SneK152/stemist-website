b57ebd019007f05aac92ea5c1f4f9f89e340e3b7
snek152 <kakanisnehil@gmail.com>
2022-06-25 11:47:09 +0530

    adding storage function

--- lib/auth/user.ts
+++ lib/auth/user.ts
@@ -1,6 +1,14 @@
-import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
+import {
+  deleteDoc,
+  doc,
+  getDoc,
+  getFirestore,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import getFirebase from "../hooks/getFirebase";
 import StudentData from "../types/StudentData";
+import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
 
 const db = getFirestore(getFirebase());
 
@@ -14,4 +22,22 @@ export const addData = async (
   uid: string
 ): Promise<void> => {
   await setDoc(doc(db, "users", uid), data);
+};
+
+export const updateData = async (
+  data: Partial<StudentData>,
+  uid: string
+): Promise<void> => {
+  await updateDoc(doc(db, "users", uid), data);
+};
+
+export const deleteData = async (uid: string): Promise<void> => {
+  await deleteDoc(doc(db, "users", uid));
+};
+
+export const uploadProfilePic = async (image: File, user: StudentData) => {
+  const storageRef = ref(getStorage(getFirebase()), `images/${user.name}`);
+  await uploadBytes(storageRef, image);
+  const url = await getDownloadURL(storageRef);
+  return url;
 };
