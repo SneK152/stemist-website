a6ad38cd807ea1b69c48e415a21bf73aa0ddd8d1
snek152 <kakanisnehil@gmail.com>
2022-06-26 06:59:11 +0530

    updating dashboard nav

--- components/auth/Dashboard.tsx
+++ components/auth/Dashboard.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { NavLinks } from "@/lib/data/navLinks";
+import useAuth from "@/lib/hooks/useAuth";
+import React, { useMemo } from "react";
+import Container from "../layout/Container";
+import PartialBanner from "../layout/PartialBanner";
 
 export default function Dashboard() {
-  return <div></div>;
+  const [user] = useAuth();
+  const dashboardNav: NavLinks = useMemo(
+    () =>
+      user === null || user === undefined
+        ? [
+            {
+              name: "Login",
+              link: "/auth/login",
+            },
+            {
+              name: "Register",
+              link: "/auth/register",
+            },
+          ]
+        : [
+            {
+              name: "Logged in as" + user.displayName,
+              link: "/dashboard",
+            },
+          ],
+    [user]
+  );
+  return (
+    <Container
+      title="Dashboard"
+      noNav
+      navTitle="Student Dashboard"
+      customNav={dashboardNav}
+    >
+      <PartialBanner title="Student Dashboard">
+        <div className="absolute">asdf</div>
+      </PartialBanner>
+    </Container>
+  );
 }
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,13 +1,11 @@
+import Dashboard from "@/components/auth/Dashboard";
+import Login from "@/components/auth/Login";
 import useAuth from "@/lib/hooks/useAuth";
 
-export default function Dashboard() {
+export default function Dash() {
   const [user, loading] = useAuth();
   if (loading) {
     return <div>Loading...</div>;
   }
-  return user === null || user === undefined ? (
-    <h1>Not logged in</h1>
-  ) : (
-    <h1>Logged in</h1>
-  );
+  return user === null || user === undefined ? <Login /> : <Dashboard />;
 }
