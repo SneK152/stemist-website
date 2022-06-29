8cb668ff7edd68e1a664873a88c1f92999641813
Yash-Singh1 <saiansh2525@gmail.com>
2022-06-28 19:24:32 -0500

    feat: ability to zoom into gallery

--- pages/about/gallery.tsx
+++ pages/about/gallery.tsx
@@ -2,9 +2,11 @@ import Container from "@/components/layout/Container";
 import PartialBanner from "@/components/layout/PartialBanner";
 import Gallery from "react-photo-gallery";
 import { useCallback } from "react";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import Image from "next/image";
 
-const photos: {
+interface Photo {
   src: string;
   srcSet?: string | string[] | undefined;
   sizes?: string | string[] | undefined;
@@ -12,7 +14,10 @@ const photos: {
   height: number;
   alt?: string | undefined;
   key?: string | undefined;
-}[] = [
+  scale?: boolean;
+};
+
+const photos: Photo[] = [
   { src: "/about/teamphoto.jpg", width: 2, height: 1 },
   { src: "/about/carousel2.png", width: 3, height: 2 },
   { src: "/about/carousel5.png", width: 3, height: 2 },
@@ -22,16 +27,23 @@ const photos: {
   { src: "/about/carousel6.png", width: 1, height: 1 },
 ];
 export default function About() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [currentPhoto, setCurrentPhoto] = useState<Photo | null>(photos[0]);
+
   const imageRenderer = useCallback(
-    ({ photo }) => (
+    ({ photo }: { photo: Photo }) => (
       <div className="m-1">
         <Image
-          alt={photo.title}
+          alt={photo.alt}
           src={photo.src}
           objectFit="cover"
           width={photo.width}
           height={photo.height}
-          className="hover:scale-110 transition-transform duration-300"
+          onClick={() => {
+            setOpen(true);
+            setCurrentPhoto(photo);
+          }}
+          className={`${photo.scale === false ? '' : 'hover:scale-110'} transition-transform duration-300 cursor-pointer`}
         />
       </div>
     ),
@@ -48,6 +60,56 @@ export default function About() {
           margin={4}
         />
       </div>
+      <Transition appear show={open} as={Fragment}>
+        <Dialog
+          as="div"
+          onClose={() => setOpen(false)}
+          className="fixed inset-0 z-50 overflow-y-auto"
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-30"></Dialog.Overlay>
+            </Transition.Child>
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-2xl p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <div>
+                  {imageRenderer({ photo: { ...currentPhoto!, width: currentPhoto!.width * 2, height: currentPhoto!.height * 2, scale: false } })}
+                </div>
+                <div className="mt-1">
+                  <button
+                    className="m-auto bg-red px-3 py-1 rounded-md bg-opacity-50 shadow-sm shadow-slate-900 relative active:top-[2px] active:shadow-none text-white font-display text-sm outline-none"
+                    onClick={() => setOpen(false)}
+                  >
+                    Back
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </Container>
   );
 }
