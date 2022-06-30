6c34822c5ce32de4c026bcaa80ab74a2e4ef0e94
Yash-Singh1 <saiansh2525@gmail.com>
2022-06-30 11:27:53 -0500

    feat: enter data for past events

--- lib/data/sessions.ts
+++ lib/data/sessions.ts
@@ -137,4 +137,237 @@ export const sessionData: Session[] = [
       },
     ],
   },
+  {
+    slug: "2021-summer",
+    title: "2021 Summer Workshop",
+    classes: [
+      {
+        teacher: "Steve Yang",
+        title: "Uncertainty Principle",
+        date: "Friday, June 18th, 2021",
+      },
+      {
+        teacher: "Chaas Kandregula",
+        title: "Vaccines",
+        date: "Friday, June 18th, 2021",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Oceanography",
+        date: "Friday, June 25th, 2021",
+      },
+      {
+        teacher: "Rohan & Arnav",
+        title: "Python",
+        date: "Friday, June 25th, 2021",
+      },
+      {
+        teacher: "Chaas Kandregula",
+        title: "Chemical Reactions",
+        date: "Friday, July 2nd, 2021",
+      },
+      {
+        teacher: "Selena Yang",
+        title: "Animal Development",
+        date: "Friday, July 2nd, 2021",
+      },
+      {
+        teacher: "Sanya",
+        title: "Combinatorics",
+        date: "Friday, July 9th, 2021",
+      },
+      {
+        teacher: "Daphne Zhu & Meghana Babu",
+        title: "Ornithology",
+        date: "Friday, July 9th, 2021",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Meteorology",
+        date: "Friday, July 16th, 2021",
+      },
+      {
+        teacher: "Rohan Fernandes",
+        title: "Machine Learning",
+        date: "Friday, July 16th, 2021",
+      },
+      {
+        teacher: "Justin Zhang",
+        title: "Beginner C",
+        date: "Friday, July 23rd, 2021",
+      },
+      {
+        teacher: "John Jou",
+        title: "A&P -  Muscoskeletal System",
+        date: "Friday, July 23rd, 2021",
+      },
+      {
+        teacher: "Chaas Kandregula",
+        title: "Nuclear Fission and Fusion",
+        date: "Friday, July 30th, 2021",
+      },
+      {
+        teacher: "Johnathan Kao",
+        title: "Astronomy",
+        date: "Friday, August 6th, 2021",
+      },
+      {
+        teacher: "Aarav",
+        title: "Python",
+        date: "Friday, August 6th, 2021",
+      },
+    ],
+  },
+  {
+    slug: "2020-winter",
+    title: "2020 Winter Workshop",
+    classes: [
+      {
+        teacher: "Aryan Patnaik",
+        title: "Immune Systems",
+        date: "Monday, December 21st, 2021",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Interpersonal Skills",
+        date: "Monday, December 21st, 2021",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Ecology",
+        date: "Wednesday, December 23rd, 2021",
+      },
+      {
+        teacher: "Chaas Kandregula",
+        title: "Periodic Table",
+        date: "Wednesday, December 23rd, 2021",
+      },
+      {
+        teacher: "Alan Yu",
+        title: "Botanical Facts",
+        date: "Sunday, December 27th, 2021",
+      },
+      {
+        teacher: "Chaas Kandregula",
+        title: "Electricity and Magnetism",
+        date: "Sunday, December 27th, 2021",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Oceanography",
+        date: "Monday, December 28th, 2021",
+      },
+      {
+        teacher: "Aryan Patnaik",
+        title: "Nervous Systems",
+        date: "Monday, December 28th, 2021",
+      },
+      {
+        teacher: "Alan Yu",
+        title: "Chemistry",
+        date: "Wednesday, December 30th, 2021",
+      },
+      {
+        teacher: "Selena Yang",
+        title: "Energy Pathways",
+        date: "Wednesday, December 30th, 2021",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Viruses",
+        date: "Friday, January 1st, 2022",
+      },
+      {
+        teacher: "Rohan Fernandes",
+        title: "Python Coding",
+        date: "Friday, January 1st, 2022",
+      },
+      {
+        teacher: "Alan Yu",
+        title: "Entomology",
+        date: "Sunday, January 3rd, 2022",
+      },
+      {
+        teacher: "Selena Yang",
+        title: "Cancer",
+        date: "Sunday, January 3rd, 2022"
+      }
+    ],
+  },
+  {
+    slug: "2020-thanksgiving",
+    title: "2020 Thanksgiving Workshop",
+    classes: [
+      {
+        teacher: "Aryan Patnaik",
+        title: "Anatomy",
+        date: "Monday, November 23rd, 2020",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Glaciers",
+        date: "Monday, November 23rd, 2020",
+      },
+      {
+        teacher: "yours truly",
+        title: "EPD",
+        date: "Tuesday, November 24th, 2020",
+      },
+      {
+        teacher: "Selena Yang",
+        title: "Genetics",
+        date: "Tuesday, November 24th, 2020",
+      },
+      {
+        teacher: "Aryan Patnaik & Sanya Badhe",
+        title: "Cryptography",
+        date: "Wednesday, November 25th, 2020",
+      },
+      {
+        teacher: "Selena Yang",
+        title: "C++",
+        date: "Wednesday, November 25th, 2020",
+      },
+      {
+        teacher: "Alan Yu",
+        title: "Ants",
+        date: "Thursday, November 26th, 2020",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Intermolecular Forces",
+        date: "Thursday, November 26th, 2020",
+      },
+      {
+        teacher: "Steve Yang",
+        title: "Meteorology",
+        date: "Friday, November 27th, 2020",
+      },
+      {
+        teacher: "Vincent Qin",
+        title: "Astronomy",
+        date: "Friday, November 27th, 2020",
+      },
+      {
+        teacher: "Aryan Patnaik",
+        title: "Marine Biology",
+        date: "Saturday, November 28th, 2020",
+      },
+      {
+        teacher: "Vincent Qin",
+        title: "Hydrology",
+        date: "Saturday, November 28th, 2020",
+      },
+      {
+        teacher: "Sanya Badhe",
+        title: "Circuits",
+        date: "Sunday, November 29th, 2020",
+      },
+      {
+        teacher: "Sanya Badhe",
+        title: "Forces",
+        date: "Sunday, November 29th, 2020",
+      },
+    ]
+  }
 ];
