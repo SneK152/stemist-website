1f45383909e457700468f040071167d30401e4ea
Yash-Singh1 <saiansh2525@gmail.com>
2022-07-07 19:34:24 -0500

    feat: try to make auth session, might break app

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,20 +1,26 @@
 import { addData, deleteData, getData, updateData } from "@/lib/auth/user";
 import { NextApiHandler } from "next";
+import db from "@/lib/serverApp";
 
 const handler: NextApiHandler = async (req, res) => {
   if (req.method === "GET") {
     const data = await getData(req.query.uid as string);
     return res.json(data);
   } else if (req.method === "POST") {
-    const body = JSON.parse(req.body);
-    await addData(
-      {
-        name: body.name,
-        profileUrl: body.profileUrl,
-      },
-      body.uid
-    );
-    return res.json({});
+    if (req.cookies.id && (await (await db.collection("ids").doc(req.cookies.id).get()).data())) {
+      await db.collection("ids").doc(req.cookies.id).delete();
+      const body = JSON.parse(req.body);
+      await addData(
+        {
+          name: body.name,
+          profileUrl: body.profileUrl,
+        },
+        body.uid
+      );
+      return res.json({});
+    } else {
+      res.redirect('/auth/register');
+    }
   } else if (req.method === "PUT") {
     const body = JSON.parse(req.body);
     await updateData(
